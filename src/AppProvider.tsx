513db5e3ea575ed896913efd001cebd0786cd175
ilanrot <ilanrot90@gmail.com>
2020-11-19 01:37:53 +0200

    add styled to provider

--- src/AppProvider.tsx
+++ src/AppProvider.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import type { ReactNode } from 'react';
+// add redux store
 import { Provider } from 'react-redux';
+// add router
 import { ConnectedRouter } from 'connected-react-router';
+// add style provider
+import AppTheme from 'styles/theme/ThemeProvider';
 import history from 'router/history';
 import configureStore from './store';
 
@@ -13,7 +17,9 @@ const store = configureStore();
 const AppProvider: React.FC<IProps> = ({ children }: IProps) => {
 	return (
 		<Provider store={store}>
-			<ConnectedRouter history={history}>{children}</ConnectedRouter>
+			<ConnectedRouter history={history}>
+				<AppTheme>{children}</AppTheme>
+			</ConnectedRouter>
 		</Provider>
 	);
 };
